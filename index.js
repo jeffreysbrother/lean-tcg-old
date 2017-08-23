8f7938b7bd5ab47949f7decf60e2e50e0f855501
James Cool <james.cool@thecontrolgroup.com>
2017-08-23 13:16:52 -0700

    initial

---
+++ index.js
@@ -0,0 +1,227 @@
+'use strict';
+const fse = require('fs-extra');
+const chalk = require('chalk');
+const path = require('path');
+const simpleGit = require('simple-git');
+const shell = require('shelljs');
+const inquirer = require('inquirer');
+
+const cwd = process.cwd();
+const ignoreHiddenFiles = /(^|\/)\.[^\/\.]/ig;
+const restrictUserInputPattern = /\b[a-zA-Z]{2}(-)\d{2,3}\b/g;
+const pathToSection = `${cwd}/source/sections`;
+const pathToConfig = `${cwd}/config.json`;
+const devInitials = require(pathToConfig).developer;
+
+let section;
+let originalDir;
+let howMany;
+let originalNamespace;
+let pathToOriginalDir;
+let pathsToNewVariations = [];
+let pathToNewDev;
+let existingDirs = [];
+let newSuffixes = [];
+let blurb;
+let newBranch;
+
+let questions = [{
+	type: 'input',
+	name: 'section',
+	message: 'What section are you working on?',
+	filter: value => {
+		return value.toLowerCase().replace(/\s/g,'');
+	},
+	validate: value => {
+		if (fse.existsSync(`${pathToSection}/${value}`)) {
+			return true;
+		} else {
+			console.log(chalk.yellow(" Section doesn't exist!"));
+			return false;
+		}
+	}
+},{
+	type: 'input',
+	name: 'originalDir',
+	message: 'Which directory do you wish to copy?',
+	filter: value => {
+		return value.toLowerCase().replace(/\s/g,'');
+	},
+	validate: value => {
+		// ensure user input is two letters, a hyphen, and 2-3 digits
+		if (value.match(restrictUserInputPattern)) {
+			return true;
+		} else {
+			console.log(chalk.yellow(' Invalid directory name!'));
+			return false;
+		}
+	}
+},{
+	type: 'number',
+	name: 'howMany',
+	message: 'How many variations would you like?',
+	filter: value => {
+		return value.toLowerCase().replace(/\s/g,'');
+	},
+	validate: value => {
+		if (!isNaN(parseFloat(value)) && isFinite(value) && value % 1 === 0) {
+			if (parseFloat(value) === 0) {
+				console.log(chalk.yellow(' What? You don\'t want that.'));
+				return false;
+			} else if (parseFloat(value) > 10) {
+				console.log(chalk.yellow(' Too many variations!'));
+				return false;
+			} else {
+				return true;
+			}
+		} else {
+			console.log(chalk.yellow(' Please enter a whole number.'));
+			return false;
+		}
+	}
+},{
+	type: 'input',
+	name: 'blurb',
+	message: 'Please enter a short branch description:',
+	filter: value => {
+		return value.toLowerCase().replace(/\s/g,'');
+	}
+}];
+
+
+(function initializing() {
+	simpleGit()
+		.checkout('master')
+		.pull('origin', 'master');
+	return true;
+})();
+
+inquirer.prompt(questions).then(answers => {
+    section = answers.section;
+    originalDir = answers.originalDir;
+		howMany = answers.howMany;
+		blurb = answers.blurb;
+
+		(function manipulation() {
+			originalNamespace = originalDir.substr(0, originalDir.indexOf('-'));
+
+			pathToOriginalDir = `${pathToSection}/${section}/${originalNamespace}/${originalDir}`;
+			pathToNewDev = `${pathToSection}/${section}/${devInitials}`;
+
+			if (!fse.existsSync(pathToNewDev)) {
+				fse.mkdirSync(pathToNewDev);
+			}
+
+			// get array of existing dirs
+			fse.readdirSync(pathToNewDev).forEach(dir => {
+				existingDirs.push(dir);
+			});
+
+			// find last existing dir
+			let lastDir = existingDirs[existingDirs.length - 1];
+
+			// get last suffix from array of existing dirs
+			if (existingDirs.length == 0) {
+				var lastSuffix = "0";
+			} else {
+				var lastSuffix = lastDir.substring(lastDir.indexOf('-') + 1, lastDir.length);
+			}
+
+			// create array of numerically next suffixes
+			for (let i = 1; i <= howMany; i++) {
+				newSuffixes.push(parseFloat(lastSuffix) + i);
+			}
+
+			// convert array of numbers to array of strings
+			let suffixesStringy = newSuffixes.map(String);
+
+			// populate array of paths to new variations, adding padding if suffix is one digit
+			suffixesStringy.forEach(suffix => {
+				if (suffix.length == 1) {
+					pathsToNewVariations.push(`${pathToNewDev}/${devInitials}-0${suffix}`);
+				} else {
+					pathsToNewVariations.push(`${pathToNewDev}/${devInitials}-${suffix}`);
+				}
+			});
+
+			newBranch = `${devInitials}_${section}_${blurb}`;
+		})();
+
+		(function checkBranch() {
+			// check if the branch already exists locally
+			if (shell.exec(`git rev-parse --verify --quiet \'${newBranch}\'`, {silent:true}).length > 0) {
+				console.log(chalk.yellow(`ERROR: local branch already exists. Terminating process.`));
+				process.exit();
+			// check if the branch already exists remotely
+			} else if (shell.exec(`git ls-remote --heads origin \'${newBranch}\'`, {silent:true}).length > 0) {
+				console.log(chalk.yellow(`ERROR: remote branch already exists. Terminating process.`));
+				process.exit();
+			} else {
+				return true;
+			}
+		})();
+
+	  (function copy() {
+			pathsToNewVariations.forEach(variation => {
+				let newFileName = path.basename(variation);
+				if (!fse.existsSync(pathToOriginalDir)) {
+		      console.log(chalk.yellow(`${originalDir} doesn't exist! Aborting.`));
+		      process.exit();
+		    } else {
+					try {
+		        fse.copySync(pathToOriginalDir, variation);
+		      } catch (err) {
+		        console.error(err);
+		      }
+				}
+			});
+	  })();
+
+	  (function rename() {
+			pathsToNewVariations.forEach(variation => {
+				fse.readdir(variation, (err, files) => {
+		      // skip hidden files
+		      files = files.filter(item => !(ignoreHiddenFiles).test(item));
+		      files.forEach(file => {
+		        let fullPath = `${variation}/${file}`;
+						let newPart = path.basename(path.dirname(fullPath));
+						let oldPart = file.substring(0, file.indexOf('.'));
+						fse.rename(fullPath, fullPath.replace(oldPart, newPart)), err => {
+							if (err) {
+								throw err;
+							}
+						};
+		      });
+		    });
+			});
+	  })();
+
+		(function message() {
+			let items = [];
+			pathsToNewVariations.forEach(variation => { items.push(path.basename(variation)) });
+			if (items.length > 1) {
+				console.log(chalk.yellow(`${howMany} variations created: ${[...items]}.`));
+			} else {
+				console.log(chalk.yellow(`${howMany} variation created: ${items}.`));
+			}
+		})();
+
+		(function git() {
+			try {
+				simpleGit()
+					.checkoutBranch(newBranch, 'master', (err, result) => {
+						console.log(chalk.yellow(`Switched to new branch ${newBranch}`));
+					})
+					.add('./*')
+					.commit(`copied ${originalDir}`, (err, result) => {
+						console.log(chalk.yellow('Changes staged and committed'));
+					})
+					.push(['-u', 'origin', `${newBranch}`], (err, result) => {
+						console.log(chalk.yellow('Pushed!'));
+					});
+			} catch (err) {
+				console.error(err);
+			}
+		})();
+
+  });
